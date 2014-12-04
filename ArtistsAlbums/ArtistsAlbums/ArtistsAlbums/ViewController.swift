3ef3b4eec6673b1e123ae89ccb7eac9937b166ae
Alexander Grafl <alexandergrafl@Alexanders-Mac.local>
2014-12-04 09:23:36 +0100

    Init for Swift project

---
+++ ArtistsAlbums/ArtistsAlbums/ArtistsAlbums/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtistsAlbums
+//
+//  Created by Alexander Grafl on 27/11/14.
+//  Copyright (c) 2014 Alexander Grafl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
