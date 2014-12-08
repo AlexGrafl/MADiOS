cd29694983ebfbafe0a01206509f1c43ae7ec7dc
Mokepon <Mokepon@Mokepons-Mac-Pro.local>
2014-12-08 14:56:11 -0800

    added all views to storyboard

---
+++ MAD2/MAD2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAD2
+//
+//  Created by Mokepon on 08/12/14.
+//  Copyright (c) 2014 Mokepon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
