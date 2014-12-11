8149da32c730642e8d063a762c78b05c2de79bb4
Mokepon <Mokepon@Mokepons-Mac-Pro.local>
2014-12-11 04:42:25 -0800

    inserting new Artist on viewDidLoad()

--- ArtistsAlbums/ArtistsAlbums/ArtistsAlbums/ArtistTableViewController.swift
+++ ArtistsAlbums/ArtistsAlbums/ArtistsAlbums/ArtistTableViewController.swift
@@ -12,6 +12,7 @@ import CoreData
 class ArtistTableViewController: UITableViewController, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
     @IBOutlet var artistTable: UITableView!
+
     lazy var managedObjectContext: NSManagedObjectContext? = {
         let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
         if let managedObjectContext = appDelegate.managedObjectContext {
@@ -20,7 +21,8 @@ class ArtistTableViewController: UITableViewController, UITableViewDataSource, N
             return nil
         }
     }()
-    lazy var artistController:NSFetchedResultsController = {
+
+    lazy var artistController: NSFetchedResultsController = {
         // Define what to be fetched
         let fetchRequest = NSFetchRequest(entityName: "Artist")
         let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
@@ -37,6 +39,13 @@ class ArtistTableViewController: UITableViewController, UITableViewDataSource, N
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let entityDescription = NSEntityDescription.entityForName("Artist", inManagedObjectContext: self.managedObjectContext!)
+        let artist = Artist()
+        artist.name = "Georg"
+        artist.label = "Label"
+
+        self.managedObjectContext!.save(nil)
 
         self.artistTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "ArtistCell")
     }
@@ -59,21 +68,17 @@ class ArtistTableViewController: UITableViewController, UITableViewDataSource, N
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("ArtistCell", forIndexPath: indexPath) as UITableViewCell
         let artist = artistController.objectAtIndexPath(indexPath) as Artist
-            
+
         cell.textLabel!.text = artist.name
             
         return cell
-    }
-
-    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        
     }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         self.performSegueWithIdentifier("showAlbum", sender: self)
         
     }
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
